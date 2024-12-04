e7a6602d71bb71e8726a89ec6a8fcbe34ebbb3b7
adhy.chandra <adhy.chandra@gmail.com>
2024-12-04 19:36:06 +0700

    add admin register

--- literatur/admin.py
+++ literatur/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import UserDetail, UserBook, customerBookmark,BookReview,Books
 # Register your models here.
+
+# class viewCustomerDb(admin.ModelAdmin):
+#     list_display= ('username','id_customer','alias','birthday')
+
+# admin.site.register(customerDb,viewCustomerDb)
+# admin.site.register(customerBook)
+admin.site.register(customerBookmark)
+admin.site.register(UserDetail)
+admin.site.register(UserBook)
+admin.site.register(BookReview)
+admin.site.register(Books)
