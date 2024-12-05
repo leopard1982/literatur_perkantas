cfe7656061d00ff6809ed70984c49b8e8062378c
adhy.chandra <adhy.chandra@gmail.com>
2024-12-05 17:18:27 +0700

    test config lagi

--- literatur/views.py
+++ literatur/views.py
@@ -4,5 +4,6 @@ from django.core.mail import send_mail
 
 # Create your views here.
 def mainPage(request):
-    # send_mail('Subject here', 'Here is the message.', 'adhy.chandra@live.co.uk', ['adhy.chandra@gmail.com'], fail_silently=False)
+    print(settings.EMAIL_PORT)
+    send_mail('Subject here Test', 'Here is the message. Test', 'adhy.chandra@live.co.uk', ['adhy.chandra@gmail.com'], fail_silently=False)
     return render(request,'index.html')
