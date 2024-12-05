c53e9e1ab86722ea12621fc99319fb863f03d6bb
adhy.chandra <adhy.chandra@gmail.com>
2024-12-05 11:14:32 +0700

    add captcha v3

--- perkantas_literatur/settings.py
+++ perkantas_literatur/settings.py
@@ -106,10 +106,10 @@ USE_TZ = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = os.environ['EMAIL_PORT']
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_USERNAME']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_HOST_USER = 'adhy.chandra@gmail.com'
+EMAIL_HOST_PASSWORD = 'Prudential1982'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
