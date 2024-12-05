94fe0cf6c88eb496d7d584a1e2cf2fc144eb6f62
adhy.chandra <adhy.chandra@gmail.com>
2024-12-05 17:21:12 +0700

    ok

--- literatur/views.py
+++ literatur/views.py
@@ -5,5 +5,5 @@ from django.core.mail import send_mail
 # Create your views here.
 def mainPage(request):
     print(settings.EMAIL_PORT)
-    send_mail('Subject here Test', 'Here is the message. Test', 'adhy.chandra@live.co.uk', ['adhy.chandra@gmail.com'], fail_silently=False)
+    # send_mail('Subject here Test', 'Here is the message. Test', 'adhy.chandra@live.co.uk', ['adhy.chandra@gmail.com'], fail_silently=False)
     return render(request,'index.html')
