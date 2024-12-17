1081ec152a51a7093cf3c3c8300d8c854c96bea7
adhy.chandra <adhy.chandra@gmail.com>
2024-12-17 09:11:08 +0700

    control apa adanya

--- literatur/views.py
+++ literatur/views.py
@@ -59,7 +59,7 @@ def bacaBuku(request):
     
     try:
         if request.method=="POST":
-            page=int(request.POST['halaman'])+2
+            page=int(request.POST['halaman'])
         else:
             page=int(request.GET['p'])
             
